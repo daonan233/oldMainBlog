aad89b77361c0b634aefaf22df08f4977133f678
daonan <2894566646@qq.com>
2023-07-15 10:35:00 +0800

    修复了一些已知的问题
    - 修复了一些已知的问题
    - 添加了页面图标

--- index.js
+++ index.js
@@ -67,6 +67,30 @@ window.onload = function(){
 	            }
 	        }());
 		
+		L2Dwidget.init({
+		        "model": {
+		            jsonPath: "https://unpkg.com/live2d-widget-model-miku/assets/miku.model.json",//这里改模型，前面后面都要改
+		            "scale": 1
+		        },
+		        "display": {
+		            "position": "left",//设置看板娘的上下左右位置
+		            "width": 200,
+		            "height": 500,
+		            "hOffset": 70,
+		            "vOffset": 70
+		        },
+		        "mobile": {
+		            "show": true,
+		            "scale": 0.5
+		        },
+		        "react": {
+		            "opacityDefault": 0.7,//设置透明度
+		            "opacityOnHover": 0.2
+		        }
+		    });
+		    window.onload = function () {
+		        $("#live2dcanvas").attr("style", "position: fixed; opacity: 0.7; left: 170px; bottom: 0px; z-index: 1; pointer-events: none;")
+		    }
 
 
 
